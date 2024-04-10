b16c3b2f1311a13ba6a0901c055e13bee9b1db47
PurandharAdigarla <purandharadigarla.com>
2024-04-10 16:47:21 +0530

    Created the basic frontend environment

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Signup" element={<Signup />} />
+        <Route path="Chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+  );
+}
+export default App;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
+
+const Header = () => {
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}></Toolbar>
+    </AppBar>
+  );
+};
+export default Header;
